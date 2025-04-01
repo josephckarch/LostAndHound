3951a3dbe2ebb4cde574a2c602ec9b921e592fc6
angelali314159 <angelali314159@gmail.com>
2025-04-01 09:48:30 -0400

    Update homepage title and header, add login and account creation links, and create new view files for login and sign-up

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -9,10 +9,11 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <header class="bg-light py-3">
         <div class="container">
-            <h1 class="text-center">Lost and Hound</h1>
+            <h1 class="text-center">Lost & Hound</h1>
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="/">Home</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,13 +22,10 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="/lost-pets">Lost Pets</a>
+                            <a class="nav-link" href="/login">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/found-pets">Found Pets</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/report-lost">Report Lost Pet</a>
+                            <a class="nav-link" href="/create-account">Create Account</a>
                         </li>
                     </ul>
                 </div>
