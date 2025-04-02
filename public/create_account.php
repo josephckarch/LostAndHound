793c47a58eba29ab389a7a2a271e9bfc017b8638
Zachary Miller <millerz@ufl.edu>
2025-04-01 23:37:28 -0400

    Added pages and CSS elements

---
+++ public/create_account.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Create Account</title>
+        <link rel="stylesheet" href="./css/styles.css">
+    </head>
+    
+    <body>
+        <header>
+            <h1>Lost & Hound</h1>
+        </header>
+    
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
