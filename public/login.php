68ef492728d051682427a45e22463a8bcfeba344
waugustin <waugustin@ufl.edu>
2025-04-03 01:21:55 -0400

    update ui for create account and login page

--- public/login.php
+++ public/login.php
@@ -10,10 +10,61 @@
     <body>
         <header>
             <h1>Lost & Hound</h1>
+            <style>
+                header{
+                    background-color: #f5e1dc;
+                    color:#DA6274;
+                }
+                .container {
+                    background-color: #f5e1dc;
+                    height: 600px;
+                    width: 600px;
+                    align-items:center;
+                   
+                }
+                .container input{
+                    width: 90%; 
+                      max-width: 300px; 
+                      padding: 10px;
+                      margin: 5px 0;
+                      font-size: 16px; 
+                      background-color:;
+                }
+                .container button {
+                    background-color:white;
+                      width: 50%; 
+                      padding: 5px;
+                      margin: 5px 0;
+                      font-size: 15px; 
+                      border-radius: 10px;
+
+                    }
+                .container p{
+                    font-size: 12px;
+                    color:black;
+                }    
+            </style>
+            
+
+            
         </header>
-    
-        
+
         <div class="container">
+         
+            <form action=/>
+            <center> <button type="button">Login with UFL</button><center>
+            <br>
+            <center><button type="button">Login with Email</button></center>
+            <br>
+            <hr>
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" value="Login">
+            <p>Do not have an account? <a href="/create_account.php">Sign Up Here</a></p>
+
+            </form>
             
         </div>
     </body>
