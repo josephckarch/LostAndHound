22d7d334cb550862f9a3db2cb2096196f47b169a
waugustin <waugustin@ufl.edu>
2025-04-05 02:19:45 -0400

    updates the login and signup with bootstrap

--- public/login.php
+++ public/login.php
@@ -5,26 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Log in</title>
         <link rel="stylesheet" href="./css/styles.css">
+        <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap" rel="stylesheet">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     
     <body>
         <?php include '../src/views/layout/header.php'; ?>
-
+   
 
         <div class="container">
-         
-            <form action=/>
-            <center> <button type="button">Login with UFL</button><center>
-            <br>
-            <center><button type="button">Login with Email</button></center>
-            <br>
-            <hr>
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email"><br><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password"><br><br>
-            <input type="submit" value="Login">
-            <p>Do not have an account? <a href="/create_account.php">Sign Up Here</a></p>
+
+            <form action=/home.php class ="login-form">
+            <div class="text-center mb-3">
+                 <button type="button" class="ufl-btn">Login with UFL</button> <br><br>
+                <button type="button" class="email-btn">Login with Email</button>
+            </div>
+            <br><hr>
+            <div class= "form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder= "Email" required><br>
+            </div>
+            <div class= "form-group; text-align: center">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password " placeholder ="Password" required><br><br>
+            </div>
+            <div class= text-center mt-3>
+                <input type="submit" class="button" value="Login"><br><br>
+            </div>
+            <p class="text-center mt-3">Do not have an account? <a href="/create_account.php">Sign Up Here</a></p>
 
             </form>
             
