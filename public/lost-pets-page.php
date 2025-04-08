70b12bc371cd19512da57791ab071499deca9836
Zachary Miller <millerz@ufl.edu>
2025-04-08 12:58:52 -0400

    Added logging out, and redirect users to log in if trying to make a post

--- public/lost-pets-page.php
+++ public/lost-pets-page.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lost & Hound - Home</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/js/scripts.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap" rel="stylesheet">
